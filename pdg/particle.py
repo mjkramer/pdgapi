9972a857e338324483b9a8b2df8298bf6d1bc864
Matt Kramer <mjkramer@users.noreply.github.com>
2023-10-23 15:48:04 -0700

    (tmp?) Tolerate multiple generic states
    See comment in code; fix K^* in table instead?

--- pdg/particle.py
+++ pdg/particle.py
@@ -67,7 +67,9 @@ class PdgParticle(PdgData):
                 if len(matches_g) == 0:
                     mcid_string = ', MC ID = %s' % self.set_mcid if self.set_mcid else ''
                     raise PdgNoDataError('Particle data for %s%s not found' % (self.pdgid, mcid_string))
-                elif len(matches_g) == 1:
+                # the ambiguity tolerance is for K^* ("duplicate" of K^*(892))
+                # which previously had entry_type of S but now has P
+                elif len(matches_g) == 1 or (len(matches_g) >= 1 and not self.api.pedantic):
                     self.cache['pdgparticle'] = matches_g[0]._mapping
                 # TODO this should indicate whether we have too many 'G' matches or too many S/P matches (from before)
                 else:
