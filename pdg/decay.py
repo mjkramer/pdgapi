894c56aa8461a7c2b8aee832da7b42360c6e22a8
Matt Kramer <mjkramer@users.noreply.github.com>
2024-02-09 11:01:45 -0800

    Restore compatibility with Python 2.7

--- pdg/decay.py
+++ pdg/decay.py
@@ -2,10 +2,7 @@
 Classes supporting decays and branching fractions/ratios.
 """
 
-from dataclasses import dataclass
-from typing import Optional, Iterable
-
-from sqlalchemy import bindparam, select, RowMapping
+from sqlalchemy import bindparam, select
 
 from pdg.data import PdgProperty
 from pdg.errors import PdgInvalidPdgIdError, PdgNoDataError
@@ -13,27 +10,27 @@ from pdg.particle import PdgParticle
 
 
 class PdgItem:
-    def __init__(self, api, pdgitem_id: int):
+    def __init__(self, api, pdgitem_id):
         self.api = api
         self.pdgitem_id = pdgitem_id
         self.cache = {}
 
     def __repr__(self):
         name = self._get_pdgitem()['name']
-        return f'PdgItem("{name}")'
+        return 'PdgItem("%s")' % name
 
-    def _get_pdgitem(self) -> RowMapping:
+    def _get_pdgitem(self):
         if 'pdgitem' not in self.cache:
             pdgitem_table = self.api.db.tables['pdgitem']
             query = select(pdgitem_table).where(pdgitem_table.c.id == bindparam('pdgitem_id'))
             with self.api.engine.connect() as conn:
                 result = conn.execute(query, {'pdgitem_id': self.pdgitem_id}).fetchone()
                 if result is None:
-                    raise PdgNoDataError(f'No PDGITEM entry for {self.pdgitem_id}')
+                    raise PdgNoDataError('No PDGITEM entry for %s' % self.pdgitem_id)
                 self.cache['pdgitem'] = result._mapping
         return self.cache['pdgitem']
 
-    def _get_targets(self) -> Iterable['PdgItem']:
+    def _get_targets(self):
         pdgitem_map_table = self.api.db.tables['pdgitem_map']
         query = select(pdgitem_map_table).where(pdgitem_map_table.c.pdgitem_id == bindparam('pdgitem_id'))
         with self.api.engine.connect() as conn:
@@ -42,7 +39,7 @@ class PdgItem:
                 yield PdgItem(self.api, row.target_id)
 
     @property
-    def has_particle(self) -> bool:
+    def has_particle(self):
         if 'has_particle' not in self.cache:
             pdgparticle_table = self.api.db.tables['pdgparticle']
             query = select(pdgparticle_table).where(pdgparticle_table.c.pdgitem_id == bindparam('pdgitem_id'))
@@ -56,42 +53,47 @@ class PdgItem:
         return self.cache['has_particle']
 
     @property
-    def particle(self) -> PdgParticle:
+    def particle(self):
         if not self.has_particle:
-            raise PdgNoDataError(f'No direct PDGPARTICLE for PDGITEM {self.pdgitem_id}')
+            raise PdgNoDataError('No direct PDGPARTICLE for PDGITEM %s' % self.pdgitem_id)
         p = self.cache['pdgparticle']
         return PdgParticle(self.api, p['pdgid'], set_mcid=p['mcid'])
 
     @property
-    def particles(self) -> Iterable[PdgParticle]:
+    def particles(self):
         if self.has_particle:
             yield self.particle
         else:
             for target in self._get_targets():
-                yield from target.particles
+                for p in target.particles:
+                    yield p
 
     @property
-    def name(self) -> str:
+    def name(self):
         return self._get_pdgitem()['name']
 
     @property
-    def name_tex(self) -> str:
+    def name_tex(self):
         return self._get_pdgitem()['name_tex']
 
     @property
-    def item_type(self) -> str:
+    def item_type(self):
         return self._get_pdgitem()['item_type']
 
 
-@dataclass
-class PdgDecayProduct:
-    item: PdgItem
-    multiplier: int
-    subdecay: Optional['PdgBranchingFraction']
+class PdgDecayProduct(object):
+    def __init__(self, item, multiplier, subdecay):
+        assert isinstance(item, PdgItem)
+        assert isinstance(multiplier, int)
+        assert subdecay is None or isinstance(subdecay, PdgBranchingFraction)
+
+        self.item = item
+        self.multiplier = multiplier
+        self.subdecay = subdecay
 
 
 class PdgBranchingFraction(PdgProperty):
-    def _get_decay(self) -> list[dict]:
+    def _get_decay(self):
         if 'pdgdecay' not in self.cache:
             pdgdecay_table = self.api.db.tables['pdgdecay']
             query = select(pdgdecay_table).where(pdgdecay_table.c.pdgid == bindparam('pdgid'))
@@ -100,7 +102,7 @@ class PdgBranchingFraction(PdgProperty):
                     result = conn.execute(query, {'pdgid': self.baseid}).fetchall()
                     self.cache['pdgdecay'] = [row._mapping for row in result]
                 except AttributeError:
-                    raise PdgInvalidPdgIdError(f'No PDGDECAY entry for {self.pdgid}')
+                    raise PdgInvalidPdgIdError('No PDGDECAY entry for ' % self.pdgid)
         return self.cache['pdgdecay']
 
     @property
